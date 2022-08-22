a94e99109c06a89f0fd9731c6584c2dfdc23ea82
주미 <105065896+Sjumi@users.noreply.github.com>
2022-08-22 10:18:14 +0900

    Create malbot.py
    frist commit

---
+++ malbot.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from streamlit_chat import message
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+import openpyxl
+
+@st.cache(allow_output_mutation=True)
+def cached_model():
+    model = SentenceTransformer('jhgan/ko-sroberta-multitask')
+    return model
+
+@st.cache(allow_output_mutation=True)
+def get_dataset():
+    df = pd.read_excel('data/concat_data2.xlsx')
+    df['embedding1'] = df['embedding1'].apply(json.loads)
+    return df
+
+model = cached_model()
+df = get_dataset()
+
+st.header('감성대화 말봇👭')
+st.markdown("[주미의 홈페이지](https://sjumiwep.shop/)")
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+with st.form('form', clear_on_submit=True):
+    user_input = st.text_input('말하기: ', '')
+    submitted = st.form_submit_button('전송')
+
+if submitted and user_input:
+    embedding = model.encode(user_input)
+
+    df['similarity1'] = df['embedding1'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
+    answer = df.loc[df['similarity1'].idxmax()]
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(answer['챗봇'])
+
+for i in range(len(st.session_state['past'])):ㄴ
+    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    if len(st.session_state['generated']) > i:
+        message(st.session_state['generated'][i], key=str(i) + '_bot')
